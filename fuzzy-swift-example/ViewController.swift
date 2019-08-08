5aff71c6a6537ccd91fadb59416884e8ddc58641
khoi <khoiracle@gmail.com>
2019-08-09 02:38:46 +0700

    Initial Commit

---
+++ fuzzy-swift-example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fuzzy-swift-example
+//
+//  Created by khoi on 8/9/19.
+//  Copyright © 2019 khoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
